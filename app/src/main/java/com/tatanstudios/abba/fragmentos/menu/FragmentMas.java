901b82e1f0a960f3536e7db225378ba44b5496a1
tatangames <54828743+tatangames@users.noreply.github.com>
2023-12-24 20:44:15 -0600

    mas cambios

---
+++ app/src/main/java/com/tatanstudios/abba/fragmentos/menu/FragmentMas.java
@@ -0,0 +1,22 @@
+package com.tatanstudios.abba.fragmentos.menu;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.tatanstudios.abba.R;
+
+public class FragmentMas extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View vista = inflater.inflate(R.layout.fragment_mas, container, false);
+
+        return vista;
+    }
+
+}
